d713532b0e9022288eb766302ab2f59b7b563d97
Adithi <adithicj06@gmail.com>
2026-06-06 16:10:01 +0530

    working

--- components/ui/badge.jsx
+++ components/ui/badge.jsx
@@ -30,7 +30,7 @@ const badgeVariants = cva(
 );
 
 function Badge({ className, variant = "default", asChild = false, ...props }) {
-  const Comp = asChild ? Slot.Root : "span";
+  const Comp = asChild ? Slot : "span";
 
   return (
     <Comp
